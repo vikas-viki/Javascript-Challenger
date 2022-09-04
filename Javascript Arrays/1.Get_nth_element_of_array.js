e1f6eb260fcc3692db5eae91cf16fab0755bc083
vikas <manojkotary@gmail.com>
2022-09-04 22:19:11 +0530

    First challenge

--- Javascript Arrays/1.Get_nth_element_of_array.js
+++ Javascript Arrays/1.Get_nth_element_of_array.js
@@ -1,11 +1,10 @@
 // Write a function that takes an array (a) and a value (n) as argument
 // Return the nth element of 'a'
 function
-myFunction
-(
-a, n
-)
-{
+    myFunction
+    (
+        a, n
+    ) {
 
-return a[n - 1]
+    return a[n - 1]
 }
