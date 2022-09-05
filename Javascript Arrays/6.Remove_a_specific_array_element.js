51d2162f449c83f162e2fedb4c0055b9db7b97db
vikas <manojkotary@gmail.com>
2022-09-05 16:58:01 +0530

    Sixth challenge

---
+++ Javascript Arrays/6.Remove_a_specific_array_element.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array (a) and a value (b) as argument
+// The function should remove all elements equal to 'b' from the array
+// Return the filtered array
+function
+    myFunction
+    (
+        a, b
+    ) {
+    return a.filter((elem) => elem !== b)
+}
