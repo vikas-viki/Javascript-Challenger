28f4ca19ed6fac8a45c1c2552607f716d7b0aad7
vikas <manojkotary@gmail.com>
2022-09-05 16:45:51 +0530

    Fifth challenge

---
+++ Javascript Arrays/5.Return_last_n_array_elements.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+function
+    myFunction
+    (
+        a, n
+    ) {
+
+    return a.slice(-n)
+}
