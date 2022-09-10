92e4ac59d723b8e81c02f3fe504e111166781ed6
vikas <manojkotary@gmail.com>
2022-09-10 17:09:39 +0530

    Fifteenth challenge

--- Javascript Arrays/15.Merge_an_arbitrary_number_of_arrays.js
+++ Javascript Arrays/15.Merge_an_arbitrary_number_of_arrays.js
@@ -1,17 +1,16 @@
 // Write a function that takes arguments an arbitrary number of arrays
 // It should return an array containing the values of all arrays
 function
-myFunction
-(
-...arrays
-)
-{
-arr=[];
-arrays.forEach((item)=>{
-   item.forEach((itemI)=>{ arr.push(itemI)})
-})
-return arr;
+    myFunction
+    (
+        ...arrays
+    ) {
+    arr = [];
+    arrays.forEach((item) => {
+        item.forEach((itemI) => { arr.push(itemI) })
+    })
+    return arr;
 
-// actual code
-// return arrays.flat();
+    // actual code
+    // return arrays.flat();
 }
